47fed77414c4b34de5f1127b197487fb2a237ed3
anypod4me <cutmountj@gmail.com>
2026-03-08 07:24:52 -0500

    Update next.config.mjs

---
+++ next.config.mjs
@@ -0,0 +1,9 @@
+// next.config.mjs
+const nextConfig = {
+  reactStrictMode: true, // Enforce React strict mode
+  compiler: {
+    styledComponents: true, // Example for styled-component support
+  },
+};
+
+export default nextConfig;
